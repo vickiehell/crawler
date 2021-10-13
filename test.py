9a016e80a9f7fc1b265671801fa3d084addb7dd8
vickiehell <2453840923@qq.com>
2021-10-13 16:45:46 +0800

    完成数据插入数据库

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-import  mysql.connector
+
+import pymysql
+from pymysql.converters import escape_string
+s=['a','b','c']
+s=escape_string(s)
+print(s)
--- 爬取b站评论.py
+++ 爬取b站评论.py
@@ -4,6 +4,7 @@ import  re
 from  bs4 import BeautifulSoup
 import time
 import pymysql
+from pymysql.converters import escape_string
 #响应头
 headers={
     "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
@@ -13,11 +14,16 @@ config={
     "host":"127.0.0.1",
     "user":"root",
     "password":"13529146337",
-    "database":"bilibili"
+    "database":"bilibili",
 }
-
-db=pymysql.connect(**config)
-cursor=db.cursor()
+#对mysql执行sql语句
+def mysql_execute(sql,params):
+    db = pymysql.connect(**config)
+    cursor=db.cursor()
+    cursor.execute(sql,params)
+    db.commit()
+    cursor.close()
+    db.close()
 #for循环控制翻页
 for i in  range(0,1):
     #从抓包工具里得到的网址
@@ -26,18 +32,23 @@ for i in  range(0,1):
     #得到返回的json文件，从json里提取消息
     data=resp.json()["data"]
     #每页有20条评论，故循环20次
-    for j in range(0,20):
-        pass
+    for j in range(0,1):
         #爬取评论的相关消息
-        content=data["replies"][j]["content"]["message"] #评论内容
-        com_address='https://t.bilibili.com/560233713032161611/#reply'+data["replies"][j]['rpid_str'] #评论链接
-        username=data["replies"][j]['member']['uname']  #作者名字
+        content=escape_string(data["replies"][j]["content"]["message"])#评论内容
+        com_address=escape_string('https://t.bilibili.com/560233713032161611/#reply'+data["replies"][j]['rpid_str']) #评论链接
+        username=escape_string(data["replies"][j]['member']['uname'])  #作者名字
         work_out_time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(data["replies"][j]['ctime'])) #发布时间（str类型）
         like_num=data["replies"][j]['like'] #点赞数量
         mid=data["replies"][j]['mid']
-        space_address=f"https://space.bilibili.com/{mid}?spm_id_from=444.42.0.0" #空间地址
-        img_address=data["replies"][j]['member']['avatar'] #图片下载地址
-
+        space_address=escape_string(f"https://space.bilibili.com/{mid}?spm_id_from=444.42.0.0") #空间地址
+        img_address=data["replies"][j]['member']['avatar'] #头像图片下载地址
+        img_resp=requests.get(img_address)
+        img_data=img_resp.content
+        #print(img_address)
+        img_name=img_address.split("/")[-1]
         #插入数据库
-        sql=f"insert into comment values(%s,%s,%s)"
+        sql="insert into comment(content,com_address,username,work_out_time,like_num,space_address,img_data) values(%s,%s,%s,%s,%s,%s,%s)"
+        params=[content,com_address,username,work_out_time,like_num,space_address,img_data]
+        mysql_execute(sql,params)
+        img_resp.close()
     resp.close()
